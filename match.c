92a8e69ea8f2812a1f484d22ebef643b3df72236
OmarElati <5796@holbertonstudents.com>
2022-12-23 20:44:31 +0100

    not read

--- match.c
+++ match.c
@@ -1,46 +1,65 @@
 #include "monty.h"
 
 /**
- * get_op - function searches for a match between opcode and text
+ * get_op_func - function searches for a match between opcode and text
  * and returns the corresponding function
- * @opcode: the opcode string
- * @argument: the argument string
+ * @line: struct containing line contents and line number
+ * @meta: struct containing all allocated memory
  *
  * Return: pointer to the matching function
  */
-void (*get_op(char *opcode, char *argument))(stack_t **, unsigned int)
+void (*get_op_func(line_t line, meta_t *meta))(stack_t **, unsigned int)
 {
-    unsigned int i = 0;
-    instruction_t ops[] = {
-        {"push", push},
-        {"pall", pall},
-        {"pint", pint},
-        {"pop", pop},
-        {"swap", swap},
-        {"add", add},
-        {"nop", nop},
-        {NULL, NULL}
-    };
+	unsigned int i = 0;
+	instruction_t ops[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", addop},
+		{"sub", subop},
+		{"div", divop},
+		{"mul", mulop},
+		{"mod", modop},
+		{"nop", nop},
+		{"pchar", pchar},
+		{"pstr", pstr},
+		{"rotl", rotlop},
+		{"rotr", rotrop},
+		{"stack", addst},
+		{"queue", addqu},
+		{NULL, NULL}
+	};
 
-    /* Check if the opcode is a comment */
-    if (opcode[0] == '#')
-        return (nop);
+	if (comment_check(line))
+		return (nop);
 
-    /* Search for a matching opcode */
-    while (ops[i].opcode)
-    {
-        if (strcmp(ops[i].opcode, opcode) == 0)
-        {
-            /* Check for a queue push opcode */
-            if (arg.flag == 1 && strcmp(ops[i].opcode, "push") == 0)
-                return (qpush);
+	while (ops[i].opcode)
+	{
+		if (strcmp(ops[i].opcode, line.content[0]) == 0)
+		{
+			push_check(line, meta, ops[i].opcode);
+			if (arg.flag == 1 &&
+			strcmp(ops[i].opcode, "push") == 0)
+			{
+				if (line.content)
+					free(line.content);
+				return (qpush);
+			}
+			free(line.content);
+			return (ops[i].f);
+		}
 
-            return (ops[i].f);
-        }
+		i++;
+	}
 
-        i++;
-    }
-
-    fprintf(stderr, "Error: unknown instruction %s\n", opcode);
-    exit(EXIT_FAILURE);
+	fprintf(stderr, "L%d: unknown instruction %s\n", line.number,
+	line.content[0]);
+	free(line.content);
+	free(meta->buf);
+	free_stack(&(meta->stack));
+	fclose(meta->file);
+	free(meta);
+	exit(EXIT_FAILURE);
 }
--- monty.h
+++ monty.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stddef.h>
 #include <unistd.h>
+#include <stdbool.h>
 #include <ctype.h>
 
 /**
@@ -22,6 +22,7 @@ typedef struct arg_s
 	int arg;
 	int flag;
 } arg_t;
+
 extern arg_t arg;
 
 /**
@@ -31,14 +32,15 @@ extern arg_t arg;
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
-		int n;
-		struct stack_s *prev;
-		struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -52,7 +54,7 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-	
+
 /**
  * struct line - contents of line and corresponding number
  * @contents: array of tokens read from the line
@@ -65,7 +67,7 @@ typedef struct line
 	unsigned int number;
 	char **content;
 } line_t;
-	
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -82,15 +84,41 @@ typedef struct meta_s
 	FILE *file;
 } meta_t;
 
-void (*get_op(char *opcode, char *argument))(stack_t **, unsigned int);
-int main(int argc, char *argv[]);
-void free_stack(stack_t **stack);
-void parseline(line_t *line, char *buffer);
-void parsefile(FILE *file);
+/* Important functions */
+void (*get_op_func(line_t line, meta_t *meta))(stack_t **, unsigned int);
 int _isalpha(int c);
+
+/* Parse functions */
+void parsefile(FILE *file);
+void parseline(line_t *line, char *buffer);
+
+/* Verification functions */
+bool comment_check(line_t line);
+void push_check(line_t line, meta_t *meta, char *opcode);
+
+/* Stack manipulation functions */
+void push(stack_t **stack, unsigned int nline);
+void pall(stack_t **stack, unsigned int nline);
+void pint(stack_t **stack, unsigned int nline);
+void pop(stack_t **stack, unsigned int nline);
+void swap(stack_t **stack, unsigned int nline);	
 void nop(stack_t **stack, unsigned int nline);
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
-int qpush(int arg1, int arg2, int arg3);
+void rotl(stack_t **stack, unsigned int nline);
+void rotlop(stack_t **stack, unsigned int nline);
+void rotrop(stack_t **stack, unsigned int nline);
+void pchar(stack_t **stack, unsigned int nline);
+void pstr(stack_t **stack, unsigned int nline);
+void free_stack(stack_t **stack);
+void nop(stack_t **stack, unsigned int nline);
+void qpush(stack_t **stack, unsigned int nline);
+void addqu(stack_t **stack, unsigned int nline);
+void addst(stack_t **stack, unsigned int nline);
+
+/* Math functions */
+void subop(stack_t **stack, unsigned int nline);
+void addop(stack_t **stack, unsigned int nline);
+void divop(stack_t **stack, unsigned int nline);
+void mulop(stack_t **stack, unsigned int nline);
+void modop(stack_t **stack, unsigned int nline);
 
 #endif /* MONTY_H */
