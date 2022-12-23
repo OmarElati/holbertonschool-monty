97fe5464f24cb8488193d73e1d283c245f6a32e7
OmarElati <5796@holbertonstudents.com>
2022-12-23 19:34:53 +0100

    test

--- monty.h
+++ monty.h
@@ -5,6 +5,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
+#include <unistd.h>
+#include <ctype.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct arg_s
+{
+	int arg;
+	int flag;
+} arg_t;
+extern arg_t arg;
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -21,23 +39,58 @@ typedef struct stack_s
 		struct stack_s *prev;
 		struct stack_s *next;
 } stack_t;
-
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
-		char *opcode;
-		void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-void (*get_op(char *opcode))(stack_t **, unsigned int);
+	
+/**
+ * struct line - contents of line and corresponding number
+ * @contents: array of tokens read from the line
+ * @number: the line number
+ *
+ * Description: contents of a line and corresponding number
+ */
+typedef struct line
+{
+	unsigned int number;
+	char **content;
+} line_t;
+	
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct meta_s
+{
+	char *buf;
+	stack_t *stack;
+	FILE *file;
+} meta_t;
+
+void (*get_op(char *opcode, char *argument))(stack_t **, unsigned int);
 int main(int argc, char *argv[]);
+void free_stack(stack_t **stack);
+void parseline(line_t *line, char *buffer);
+void parsefile(FILE *file);
+int _isalpha(int c);
+void nop(stack_t **stack, unsigned int nline);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
+int qpush(int arg1, int arg2, int arg3);
 
 #endif /* MONTY_H */
--- opcode.c
+++ opcode.c
@@ -1,60 +1,57 @@
 #include "monty.h"
 
 /**
- * push - add a new element to the top of the stack
- * @stack: pointer to the top of the stack
- * @line_number: line number of the command being processed
+ * push - pushes a node to the top of stack
+ * @stack: pointer to the head node pointer of the stack
+ * @line_number: the line number
  *
- * Return: Empty
+ * Return: nothing
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-	int x = 0;
-	stack_t *new;
+    stack_t *new;
 
-	/* Read the value to push from the command line */
-	if (stack == NULL)
-	{
-		fprintf(stderr, "L%u: invalid push argument\n", line_number);
-		exit(EXIT_FAILURE);
-	}
+    /* Check if the stack exists */
+    if (stack == NULL)
+    {
+        fprintf(stderr, "L%u: stack not found\n", line_number);
+        exit(EXIT_FAILURE);
+    }
 
-	/* Allocate memory for the new element */
-	new = malloc(sizeof(stack_t));
-	if (new == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
+    /* Allocate memory for the new node */
+    new = malloc(sizeof(stack_t));
+    if (new == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        free_stack(stack);
+        exit(EXIT_FAILURE);
+    }
 
-	/* Initialize the new element and add it to the top of the stack */
-	new->n = x;
-	new->prev = NULL;
-	new->next = *stack;
-	if (*stack != NULL)
-		(*stack)->prev = new;
-	*stack = new;
+    /* Set the values for the new node */
+    new->next = *stack;
+    new->prev = NULL;
+    new->n = arg.arg;
 
+    /* Update the pointers of the adjacent nodes */
+    if (*stack)
+        (*stack)->prev = new;
+    *stack = new;
 }
 
 /**
- * pall - print all the values on the stack
- * @stack: pointer to the top of the stack
- * @line_number: line number of the command being processed (unused)
+ * pall - prints the data of all nodes in stack
+ * @stack: pointer to the head node pointer of stack
+ * @line_number: the line number
  *
- * Return: Empty
+ * Return: Nothing.
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *current;
+    stack_t *node;
 
-	(void) line_number;
+    (void)line_number;
 
-	/* Print the values on the stack, from the top down */
-	current = *stack;
-	while (current != NULL)
-	{
-		printf("%d\n", current->n);
-		current = current->next;
-	}
+    /* Iterate through the stack and print the data of each node */
+    for (node = *stack; node; node = node->next)
+        printf("%d\n", node->n);
 }
