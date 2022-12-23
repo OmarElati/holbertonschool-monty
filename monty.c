97fb938da4f1624f71c48b74b4c2840f16a6e6a9
OmarElati <5796@holbertonstudents.com>
2022-12-23 07:37:36 +0100

    main file

--- monty.c
+++ monty.c
@@ -8,20 +8,20 @@
 */
 int main(int argc, char *argv[])
 {
-    FILE *file;
+	FILE *file;
 
-    /* Check for the correct number of arguments */
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+	/* Check for the correct number of arguments */
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-    /* Open the input file */
-    file = fopen(argv[1], "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	/* Open the input file */
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 }
