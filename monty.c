c2fd8910875493f6828346040542f18699a4d3a8
OmarElati <5796@holbertonstudents.com>
2022-12-23 13:52:14 +0100

    main file

--- monty.c
+++ monty.c
@@ -16,7 +16,6 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
 	/* Open the input file */
 	file = fopen(argv[1], "r");
 	if (file == NULL)
@@ -24,4 +23,5 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
+	return (EXIT_FAILURE);
 }
