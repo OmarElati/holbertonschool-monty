c3b2a7ce7f13d1e01ecb9450b44259fc11783f2e
OmarElati <5796@holbertonstudents.com>
2023-01-07 16:44:12 +0100

    new_code_test

--- monty.c
+++ monty.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+        fprintf(stderr, "USAGE: monty file\n", argv[0]);
         return EXIT_FAILURE;
     }
 
