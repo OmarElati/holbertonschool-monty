3323e4b61cf168c03521dd983af0ceaaef649772
OmarElati <5796@holbertonstudents.com>
2023-01-07 15:24:50 +0100

    new_code

--- monty.c
+++ monty.c
@@ -6,8 +6,8 @@
 
 typedef struct stack
 {
-    int data[MAX_STACK_SIZE];
-    int top;
+	int data[MAX_STACK_SIZE];
+	int top;
 } Stack;
 
 void push(Stack *stack, int value);
@@ -15,52 +15,52 @@ void pall(const Stack *stack);
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s file\n", argv[0]);
-        return EXIT_FAILURE;
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s file\n", argv[0]);
+		return EXIT_FAILURE;
+	}
 
-    FILE *fp = fopen(argv[1], "r");
-    if (!fp)
-    {
-        fprintf(stderr, "Error: failed to open file %s\n", argv[1]);
-        return EXIT_FAILURE;
-    }
+	FILE *fp = fopen(argv[1], "r");
+	if (!fp)
+	{
+		fprintf(stderr, "Error: failed to open file %s\n", argv[1]);
+		return EXIT_FAILURE;
+	}
 
-    Stack stack = { .top = -1 };
+	Stack stack = { .top = -1 };
 
-    char line[256];
-    int line_number = 0;
-    while (fgets(line, sizeof(line), fp))
-    {
-        line_number++;
+	char line[256];
+	int line_number = 0;
+	while (fgets(line, sizeof(line), fp))
+	{
+		line_number++;
 
-        char *command = strtok(line, " \n");
-        if (!command)
-            continue;
+		char *command = strtok(line, " \n");
+		if (!command)
+			continue;
 
-        if (strcmp(command, "push") == 0)
-        {
-            char *arg = strtok(NULL, " \n");
-            if (!arg)
-            {
-                fprintf(stderr, "L%d: usage: push integer\n", line_number);
-                return EXIT_FAILURE;
-            }
+		if (strcmp(command, "push") == 0)
+		{
+			char *arg = strtok(NULL, " \n");
+			if (!arg)
+			{
+				fprintf(stderr, "L%d: usage: push integer\n", line_number);
+				return EXIT_FAILURE;
+			}
 
-            int value = atoi(arg);
-            push(&stack, value);
-        }
-        else if (strcmp(command, "pall") == 0)
-            pall(&stack);
-        else
-        {
-            fprintf(stderr, "L%d: unknown instruction %s\n", line_number, command);
-            return EXIT_FAILURE;
-        }
-    }
+			int value = atoi(arg);
+			push(&stack, value);
+		}
+		else if (strcmp(command, "pall") == 0)
+			pall(&stack);
+		else
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, command);
+			return EXIT_FAILURE;
+		}
+	}
 
-    fclose(fp);
-    return 0;
+	fclose(fp);
+	return 0;
 }
