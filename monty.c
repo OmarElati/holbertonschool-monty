d1816451bc63c109696ed5f1089590ae6120af30
OmarElati <5796@holbertonstudents.com>
2023-01-07 16:54:15 +0100

    new_code_test

--- monty.c
+++ monty.c
@@ -1,62 +1,58 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 #include "monty.h"
 
 /**
  * main - ..
  * @argc: ..
- * @argv: ..
+ * @argc: ..
  * Return: ..
 */
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		return EXIT_FAILURE;
-	}
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        return EXIT_FAILURE;
+    }
 
-	FILE *fp = fopen(argv[1], "r");
-	if (!fp)
-	{
-		fprintf(stderr, "Error: Can't open file HoLbErToN\n");
-		return EXIT_FAILURE;
-	}
+    FILE *fp = fopen(argv[1], "r");
+    if (!fp)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
 
-	stack_t *stack = NULL;
+    stack_t *stack = NULL;
 
-	instruction_t ops[] = {
-		{"push", push},
-		{"pall", pall},
-		{NULL, NULL}
-	};
+    instruction_t ops[] = {
+        {"push", push},
+        {"pall", pall},
+        {NULL, NULL}
+    };
 
-	char line[256];
-	int line_number = 0;
-	while (fgets(line, sizeof(line), fp))
-	{
-		line_number++;
+    char line[256];
+    int line_number = 0;
+    while (fgets(line, sizeof(line), fp))
+    {
+        line_number++;
 
-		char *command = strtok(line, " \n");
-		if (!command)
-			continue;
+        char *command = strtok(line, " \n");
+        if (!command)
+            continue;
 
-		int i;
-		for (i = 0; ops[i].opcode; i++)
-			if (strcmp(command, ops[i].opcode) == 0)
-				break;
+        int i;
+        for (i = 0; ops[i].opcode; i++)
+            if (strcmp(command, ops[i].opcode) == 0)
+                break;
 
-		if (ops[i].opcode)
-			ops[i].f(&stack, line_number);
-		else
-		{
-			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, command);
-			return EXIT_FAILURE;
-		}
-	}
+        if (ops[i].opcode)
+            ops[i].f(&stack, line_number);
+        else
+        {
+            fprintf(stderr, "L%d: unknown instruction %s\n", line_number, command);
+            return EXIT_FAILURE;
+        }
+    }
 
-	fclose(fp);
-	return 0;
+    fclose(fp);
+    return 0;
 }
